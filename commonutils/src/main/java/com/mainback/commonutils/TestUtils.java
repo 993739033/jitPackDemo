ba197a54655f18537af2bd67b33ea1468ee71828
MainBack <993739033@qq.com>
2020-12-10 11:50:29 +0800

    Initial commit

---
+++ commonutils/src/main/java/com/mainback/commonutils/TestUtils.java
@@ -0,0 +1,13 @@
+package com.mainback.commonutils;
+
+import android.util.Log;
+
+/**
+ * @author Loki_Zhou
+ * @Date 2020/12/10
+ **/
+public class TestUtils {
+    public static void PrintTest() {
+        Log.d("DD", "DDDDDD");
+    }
+}
